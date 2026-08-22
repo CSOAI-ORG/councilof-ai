05cfc0ff0bf6d30f56531cc9a05af4f93f67d5b3
Nicholas Templeman <nicholas@csoai.org>
2026-08-22 06:31:15 +0100

    fix(hygiene): restore App.tsx part 1

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,1 +1,8 @@
-PLACEHOLDER_WILL_REPLACE
+import { Toaster } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import NotFound from "@/pages/NotFound";
+import { Route, Switch, useLocation, Redirect } from "wouter";
+import { useEffect, lazy, Suspense } from "react";
+import ErrorBoundary from "./components/ErrorBoundary";
+import { SectionLoader } from "./components/PageLoader";
+import { ThemeProvider } from "./contexts/ThemeContext";
