c67fe62eb64f88ada60153e7ce275e9d9482d62d
Nicholas Templeman <nicholas@csoai.org>
2026-08-25 00:47:28 +0100

    fix: leftover /get-certified door 308 to lobby

---
+++ functions/get-certified.ts
@@ -0,0 +1,14 @@
+/**
+ * GET /get-certified and /get-certified/ - 308 to the lobby.
+ * Leftover certification door. Measurement, not certification.
+ * Do not 308 onto /get-certified/.
+ */
+export function onRequest() {
+  return new Response(null, {
+    status: 308,
+    headers: {
+      location: "/?lobby=home",
+      "cache-control": "public, max-age=300",
+    },
+  });
+}
