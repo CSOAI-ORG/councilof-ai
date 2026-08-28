7f045044bebe17cae3e40e96129c16e11edbeb75
Nicholas Templeman <nicholas@csoai.org>
2026-08-28 16:10:34 +0100

    fix(doors): leftover marketing CTAs onto /os Assess and /os home

--- client/src/pages/WorldGlobe.tsx
+++ client/src/pages/WorldGlobe.tsx
@@ -1,10 +1,1 @@
-import { useEffect, useRef, useState } from "react";
-import { chargeSovereign } from "../lib/sovCharge";
-import { askSovereign } from "../lib/sovAsk";
-import { sovActions, describeActions } from "../lib/sovAgent";
-import { flyAndConvene, drive } from "../lib/globeDrive";
-import { REGIONS } from "../lib/locale";
-import { Link } from "wouter";
-import CouncilNav from "../components/CouncilNav";
-import AISystemNotice from "../components/AISystemNotice";
-import { LAYER0_NODES, PERSONA_TOURS, STATUS_COLOR, COUNTS, type Persona } from "../data/layer0Nodes";
+PLACEHOLDER_WILL_FAIL
